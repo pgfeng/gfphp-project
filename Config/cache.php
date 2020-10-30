9774346766ca098f0df704d7cdf62f929a638008
PGF <592472116@qq.com>
2020-10-30 11:08:25 +0800

    Update cache.php
    错误修改

--- Config/cache.php
+++ Config/cache.php
@@ -1,17 +1,21 @@
 <?php
 
+use GFPHP\Cache\File as FileAlias;
 return array(
 
     //-- Default Cache Space
     'default_space' => 'public',
 
-    //======= CACHE DRIVER CONFIG START =====//                                             可以参考drivers下的缓存驱动
+    //-- default lifetime
 
-//    'driver' => \GFPHP\Cache\memcached::class,
+    'lifetime' => '0',       //过期设置为一个小时，过期后自动清除
+
+    //======= CACHE DRIVER CONFIG START =====//                                             可以参考drivers下的缓存方法
+
+//    'driver' => \GFPHP\Cache\Memcached::class,           //目前不可以将SESSION存放，原因不明
 //    'driver' => \GFPHP\Cache\DB::class,
-//
-    'driver' => \GFPHP\Cache\File::class,
-//    'driver' => \GFPHP\Cache\redis::class,
+
+    'driver' => FileAlias::class,
 
     //======= CACHE DRIVER CONFIG OVER  =====//
 
@@ -36,15 +40,6 @@ return array(
     'table' => 'caches',
 
     //-- cache table create status
-    'create' => '1',
+    'create' => 1
     //=======   DB CONFIG OVER  =======//
-
-
-    //========== redis CONFIG START =======//
-    //-- redis host
-    'redis_host' => '127.0.0.1',
-
-    //-- redis port
-    'redis_port' => '6379',
-    //=======   redis CONFIG OVER  =======//
 );
